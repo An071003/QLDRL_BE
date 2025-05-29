5882aa7b6a9b7f18e07fe6efa38f9b3d2e4fcc13
An071003 <pekkidjj123@gmail.com>
2025-05-29 18:56:40 +0700

    Update cookie options in AuthController to ensure secure and consistent behavior

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -16,11 +16,12 @@ class AuthController {
         const token = AuthController.signToken(payload);
         const cookieOptions = {
             expires: new Date(
-                Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                Date.now() + (process.env.JWT_COOKIE_EXPIRES || 90) * 24 * 60 * 60 * 1000
             ),
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "none",
+            secure: true,
+            sameSite: 'none',
+            path: '/',
         };
 
         res.cookie("token", token, cookieOptions);
