244491b5963324afbf3154d2301796e6b9bb3121
An071003 <pekkidjj123@gmail.com>
2025-05-30 00:20:24 +0700

    Update cookie options in createSendToken method for environment-specific settings

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,8 +19,8 @@ class AuthController {
                 Date.now() + (process.env.JWT_COOKIE_EXPIRES || 90) * 24 * 60 * 60 * 1000
             ),
             httpOnly: true,
-            secure: true,
-            sameSite: 'none',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             path: '/',
         };
         console.log("Cookie Options:", cookieOptions);
