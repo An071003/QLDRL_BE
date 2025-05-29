a52430286a8fa344c11ca8238f6de369affcd736
An071003 <pekkidjj123@gmail.com>
2025-05-29 14:59:47 +0700

    chore: Add additional logging for database connection environment variables in db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 console.log("🧪 DB_SSL_CA =", process.env.DB_SSL_CA);
+console.log("🧪 DB_HOST =", process.env.DB_HOST);
+console.log("🧪 DB_PORT =", process.env.DB_PORT);
+console.log("🧪 DB_NAME =", process.env.DB_NAME);
+console.log("🧪 DB_USER =", process.env.DB_USER);
+console.log("🧪 DB_PASSWORD", process.env.DB_PASSWORD);
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,     
   process.env.DB_USER,     
