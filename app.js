5a2bcf189462eb80c4b84861fe6cc23c1750d8cf
An071003 <pekkidjj123@gmail.com>
2025-05-29 10:45:41 +0700

    feat: Add health check endpoint to verify server status

--- app.js
+++ app.js
@@ -32,7 +32,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
+app.get("/", (req, res) => {
+  console.log("GET / - server is alive");
+  res.send("Server is running!");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/roles", roleRoutes);
 app.use('/api/permissions', permissionRoutes);
